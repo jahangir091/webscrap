6a2a8178efee50251401937dfbe5e061716735e0
jahangir091 <jahangir.cse09@gmail.com>
2019-08-06 23:24:01 +0600

    added mysql db

--- scrapyProject/scrapyProject/settings.py
+++ scrapyProject/scrapyProject/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'scrapyProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 # DATABASES = {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'caplugsa_webscrap1',
@@ -90,16 +90,16 @@ DATABASES = {
 #         'CONN_TOUT': 900,
 #     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'caplugsa_webscrap1',
-#         'USER': 'caplugsa_admin',
-#         'PASSWORD': 'admin',
-#         'HOST': '198.46.83.220',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'caplugsa_webscrap1',
+        'USER': 'caplugsa_admin',
+        'PASSWORD': 'admin',
+        'HOST': '198.46.83.220',
+        'PORT': '3306',
+    }
+}
 
 
 
