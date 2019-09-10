823ce9d3b6efd5fc8c7ea25c67671a687b520776
jahangir091 <jahangir.cse09@gmail.com>
2019-09-11 02:14:12 +0600

    removed unitprice_bk

--- scrapyProject/store_data/models.py
+++ scrapyProject/store_data/models.py
@@ -140,7 +140,6 @@ class Pricing(models.Model):
     """
     quantity = models.CharField(max_length=30, blank=True, null=True)
     unitPprice = models.DecimalField(max_digits=20, decimal_places=4)
-    unitPprice_bk = models.CharField(max_length=20, blank=True, null=True)
     variant = models.ForeignKey(Variant, related_name='pricing', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
